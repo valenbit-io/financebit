935d908984977b3b43548aa37415530b3b51dfbf
jrvalencia-bit <jrazovalencia@gmail.com>
2026-01-20 17:26:52 -0600

    Migracion final a ValenbitApp.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './ValenbitApp.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from './context/ThemeContext'
