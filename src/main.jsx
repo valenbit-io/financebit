1cda8373151194adebe2ed5d3da7fdc6d3fd2bec
jrvalencia-bit <jrazovalencia@gmail.com>
2026-01-20 17:40:12 -0600

    Fix: Recrear App.jsx con nombre correcto y Footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './ValenbitApp.jsx'
+import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from './context/ThemeContext'
